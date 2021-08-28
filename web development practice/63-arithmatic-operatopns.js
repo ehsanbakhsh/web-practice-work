78ec9f76126d9be9619e1b7bd15fed10fd51f938
ehmusman <ehmusman@gmail.com>
2021-08-28 15:36:21 +0500

    added 60 to 67 files

---
+++ web development practice/63-arithmatic-operatopns.js
@@ -0,0 +1,15 @@
+var a = 10;
+var b = 5;
+var c = a + b;
+var d = a - b;
+var e = a * b;
+var f = a / b;
+var g = a % b;
+var h = a ** b;
+
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
