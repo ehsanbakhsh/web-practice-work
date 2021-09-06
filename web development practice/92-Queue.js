a3ba18cb007526812149fa13c44b197d27096ba9
ehmusman <ehmusman@gmail.com>
2021-09-06 16:00:40 +0500

    added files 92 and 93 html and js

---
+++ web development practice/92-Queue.js
@@ -0,0 +1,18 @@
+var foods = [];
+
+
+foods.push("Pizza");
+foods.push("Burger");
+foods.push("Sandwich");
+
+
+console.log(foods);
+console.log(foods.length);
+
+var a1 = foods.shift();
+console.log(foods);
+console.log(foods.length);
+
+var a2 = foods.shift();
+console.log(foods);
+console.log(foods.length);
---
+++ web development practice/93-unshift-function.js
@@ -0,0 +1,11 @@
+var foods = ["Pizza" , "Burger" , "Snacks"];
+
+console.log(foods);
+
+foods.unshift("Sandwich");
+
+console.log(foods);
+console.log(foods[0]);
+console.log(foods[1]);
+console.log(foods[2]);
+console.log(foods[3]);
