df38b73538d64988b080ccd6e9d4062d3993e391
ehmusman <ehmusman@gmail.com>
2021-09-10 15:53:22 +0500

    Added files 108 to 112

---
+++ web development practice/108-math.random()-function.js
@@ -0,0 +1,11 @@
+var a = Math.random();
+console.log(a);
+
+var num1 = a * 6;
+console.log("Num1 = " + num1);
+
+var num2 = num1 + 1;
+console.log("Num2 = " + num2);
+
+var num3 = Math.floor(num2);
+console.log("Num3 = " + num3);
---
+++ web development practice/110-Date.js
@@ -0,0 +1,8 @@
+var a = new Date();
+console.log(a);
+
+var b = new Date("2021 9 3");
+console.log(b);
+
+var c = new Date("2021/9/3 11:25:50");
+console.log(c);
